438c9115863a68ff86d3faf78e1679872901280a
dflaux <flauxdonovan@gmail.com>
2024-10-12 18:12:43 +0200

    core version in config

--- data/js/version.js
+++ data/js/version.js
@@ -1,8 +1,9 @@
-const filePathVersion = '../config/version.txt'
+const filePathWebVersion = '../config/version.txt'
+const CoreVersion = ''
 
 
-export function getVersion() {
-    fetch(filePathVersion)
+export function getWebVersion() {
+    fetch(filePathWebVersion)
         .then(response => {
             if (!response.ok) {
             throw new Error('Erreur lors du chargement du fichier');
@@ -10,10 +11,20 @@ export function getVersion() {
             return response.text(); // Lit le contenu comme texte
         })
         .then(data => {
-            document.getElementById('web-version').textContent = " Web Version: " + data; // Affiche le texte dans l'élément <pre>
+            document.getElementById('web-version').textContent = " Web Version: " + data;
         })
         .catch(error => {
             console.error('Erreur :', error);
             document.getElementById('web-version').textContent = "Erreur lors de l'affichage de la version.";
         });
+}
+
+export function getCoreVersion(data) {
+    const version = data;
+    console.log(version)
+    if (version) {
+        document.getElementById('core-version').textContent = "Core Version: " + version;
+    } else {
+      document.getElementById('core-version').textContent = "Erreur lors de l'affichage de la version.";  
+    }
 }
