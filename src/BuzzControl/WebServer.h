3b097de19b281c51edee0aad660f2a4064e8a9db
cyril@coupel.net <cyril@coupel.net>
2024-11-08 17:40:09 +0100

    Gestion backgroud image

--- src/BuzzControl/WebServer.h
+++ src/BuzzControl/WebServer.h
@@ -6,9 +6,20 @@
 
 static const char* WEB_TAG = "WEBSERVER";
 
+void addCorsHeaders(AsyncWebServerResponse* response) {
+    response->addHeader("Access-Control-Allow-Origin", "*");
+    response->addHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    response->addHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    response->addHeader("Access-Control-Allow-Credentials", "true");
+}
+
 void w_handleRoot(AsyncWebServerRequest *request) {
   ESP_LOGI(WEB_TAG, "Handling root request");
-  request->send(200, "text/plain", "hello from BuzzControl!");
+  AsyncWebServerResponse *response = request->beginResponse(200, "text/html", "hello from BuzzControl!");
+    addCorsHeaders(response);
+
+  request->send(response);
+
   digitalWrite(ledPin, LOW);
 }
 
@@ -23,7 +34,10 @@ void w_handleNotFound(AsyncWebServerRequest *request) {
       message += " " + request->argName(i) + ": " + request->arg(i) + "\n";
   }
   
-  request->send(404, "text/plain", message);
+  AsyncWebServerResponse *response = request->beginResponse(404, "text/plain", message);
+    addCorsHeaders(response);
+
+  request->send(response);
 }
 
 void w_handleReboot(AsyncWebServerRequest *request) {
@@ -42,6 +56,42 @@ void w_handleRedirect(AsyncWebServerRequest *request) {
     request->redirect("http://buzzcontrol.local/html/config.html");
 }
 
+void w_handleUploadComplete(AsyncWebServerRequest *request) {
+    AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", "Upload terminé");
+    addCorsHeaders(response);
+
+    request->send(response);
+}
+
+
+void w_handleUploadFile(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
+    static File file;
+    static size_t totalSize = 0;
+    if(!index) { // Début de l'upload
+        ESP_LOGI(WEB_TAG,"Début de l'upload du Background");
+        file = LittleFS.open("/files/background.jpg", "w");
+        totalSize=0;
+        if(!file) {
+            ESP_LOGI(WEB_TAG,"Échec de l'ouverture du fichier background en écriture");
+            return;
+        }
+    }
+
+    if(file && len) { // Écriture des données
+        file.write(data, len);
+        totalSize+=len;
+    }
+
+    if(final) { // Fin de l'upload
+        if(file) {
+            ESP_LOGD(WEB_TAG,"Upload background terminé: %i", totalSize);
+            file.close();
+        }
+    }
+}
+
+
+
 void startWebServer() {
     String ROOT="/";
     if (LittleFS.exists("/CURRENT")) {
@@ -50,6 +100,7 @@ void startWebServer() {
     }
 
     server.serveStatic("/", LittleFS, ROOT.c_str());
+    server.onNotFound(w_handleNotFound);
 
     server.on("/index.html", w_handleRedirect);
 
@@ -63,6 +114,8 @@ void startWebServer() {
     server.on("/reset", HTTP_GET, w_handleReset);
     server.on("/reboot", HTTP_GET, w_handleReboot);
     
+    server.on("/background", HTTP_POST, w_handleUploadComplete, w_handleUploadFile);
+
     ws.onEvent(onWsEvent);
     server.addHandler(&ws);
 
