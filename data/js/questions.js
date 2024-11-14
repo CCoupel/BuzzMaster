11d1a77ab3fd7bf2bbad2269c8e664f644c11b22
dflaux <flauxdonovan@gmail.com>
2024-11-14 18:15:13 +0100

    show existing questions in questions.html

--- data/js/questions.js
+++ data/js/questions.js
@@ -20,6 +20,7 @@ async function sendForm(formId, actionUrl) {
 
             alert("Question crée avec succès !");
             console.log("Réponse du serveur :", responseData);
+            questionList();
 
         } catch (error) {
             console.error("Erreur lors de l'envoi:", error);
@@ -28,6 +29,42 @@ async function sendForm(formId, actionUrl) {
     });
 }
 
+async function questionList() {
+    try {
+        const response = await fetch('http://buzzcontrol.local/questions');
+        
+        if (!response.ok) {
+            throw new Error('Erreur réseau : ' + response.statusText);
+        }
+        
+        const questions = await response.json();
+        
+        const container = document.getElementById('questions-container');
+        
+        container.innerHTML='';
+
+        Object.keys(questions).forEach(key => {
+            const questionData = questions[key];
+            
+            const questionDiv = document.createElement('div');
+            questionDiv.className = 'question-item';
+
+            questionDiv.innerHTML = `
+                <p><strong>ID:</strong> ${questionData.ID}</p>
+                <p><strong>Question:</strong> ${questionData.QUESTION}</p>
+                <p><strong>Réponse:</strong> ${questionData.ANSWER}</p>
+                <p><strong>Points:</strong> ${questionData.POINTS}</p>
+                <p><strong>Temps:</strong> ${questionData.TIME} secondes</p>
+            `;
+            
+            container.appendChild(questionDiv);
+        });
+    } catch (error) {
+        console.error('Erreur lors de la récupération des questions :', error);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     sendForm('question-form', 'http://buzzcontrol.local/questions');
+    questionList();
 });
